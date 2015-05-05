65eb7ff943a90220313c1a015d983d0b41e7d90b
Louisvh <Louis.van.harten@gmail.com>
2015-05-05 15:05:03 +0200

    changed main_test for easier profiling

--- main_test.py
+++ main_test.py
@@ -9,27 +9,32 @@ import pycuda.gpuarray as gpuarray
 def nearest_2power(n):
     return np.power(2.,(np.ceil(np.log2(n))))
 
-def main():
+@profile
+def main(context, stream, plan1, N1, N2):
+    #N1 = # ffts applied
+    #N2 = dim of ffts
+    x = np.linspace(0, 2 * np.pi, N2)
 
-    x = np.linspace(0, 2 * np.pi, 400)
-    y1 = np.sin(2 * x)
-    y1 = np.concatenate((y1,np.zeros(112)))
-    y1 = y1.reshape(1,512)
-    y2 = np.sin(10 * x)
-    y2 = np.concatenate((y2,np.zeros(112)))
-    y2 = y2.reshape(1,512)
-    y = np.concatenate(((y1),(y2)),0).transpose()
-    yim= np.zeros((512,2))
+    y = np.sin(2 * x)
+    y = np.concatenate((y,np.zeros(nearest_2power(N2)-N2)))
+    y = y.reshape(1,512)
 
+    for i in xrange(49): #append 49 sines
+        yi = np.sin(i * x)
+        yi = np.concatenate((yi,np.zeros(nearest_2power(N2)-N2)))
+        yi = yi.reshape(1,nearest_2power(N2))
+        y = np.concatenate(((y),(yi)),0)
+        
+    y = y.transpose()
+    yim= np.zeros(y.shape)
     y = np.array(y,np.float64)
     yw = y.transpose()
     yimw = yim.transpose()
 
-    a = np.fft.fft(y,int(nearest_2power(400)),0)
-    b = np.real(np.fft.ifft(a,int(nearest_2power(400)),0))
-    aw = np.fft.fft(yw,int(nearest_2power(400)),1)
-    bw = np.real(np.fft.ifft(aw,int(nearest_2power(400)),1))
+    aw = np.fft.fft(yw,int(nearest_2power(N2)),1)
+    bw = np.real(np.fft.ifft(aw,int(nearest_2power(N2)),1))
 
+    '''    
     #plot numpy fft results
     f, axarr = plt.subplots(6, sharex=False)
     axarr[0].plot(y)
@@ -38,24 +43,16 @@ def main():
     axarr[1].set_title('output np.fft(input)')
     axarr[2].plot(bw.transpose())
     axarr[2].set_title('output np.ifft(np.fft(input))')
+    '''
 
-    #init cuda
-    cuda.init()
-    context = make_default_context()
-    stream = cuda.Stream()
-    plan2 = Plan((512, 2), dtype=np.float64, context=context, stream=stream, fast_math=False)
-    plan1 = Plan((512), dtype=np.float64, context=context, stream=stream, fast_math=False)
-
-    #move data to gpu
     gpu_testmat = gpuarray.to_gpu(y)
     gpu_testmatim = gpuarray.to_gpu(yim)
     plan1.execute(gpu_testmat, gpu_testmatim, batch=2) 
     c = gpu_testmat.get() #get fft result
     plan1.execute(gpu_testmat, gpu_testmatim, inverse=True, batch=2) 
     d = np.real(gpu_testmat.get()) #get ifft result
-
-    print c.shape
-
+    
+    '''
     #plot cuda fft results
     axarr[3].plot(y)
     axarr[3].set_title('input padded')
@@ -69,8 +66,21 @@ def main():
     print y
     print "c= "
     print c
+    '''
 
-    #destroy cuda context
-    context.pop()
+#set dimensions
+N1 = 50
+N2 = 400
+N = 1000
 
-main()
+#init cuda
+cuda.init()
+context = make_default_context()
+stream = cuda.Stream()
+plan1 = Plan(int(nearest_2power(N2)), dtype=np.float64, context=context, stream=stream, fast_math=False)
+#do operation N times for profiling purposes
+for i in xrange(N):
+    main(context=context, stream=stream, plan1=plan1, N1=N1, N2=N2)
+
+#destroy cuda context
+context.pop()
